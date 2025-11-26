791a6784e141a16e9e522654488eebbfbcba4ac4
Harry <lihawl@hotmail.com>
2025-11-26 10:07:16 +0100

    add default Druid.sh which contain default config to startup

---
+++ bin/Druid.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+#source /cvmfs/sw.hsf.org/key4hep/setup.sh
+export LIBGL_ALWAYS_SOFTWARE=1
+#export LIBGL_ALWAYS_INDIRECT=1
+
+
+# Absolute path to the directory that this script lives in
+SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
+
+# Run the sibling executable "Druid"
+"$SCRIPT_DIR/Druid" -coll.caloHit.filterOutSuffix Digi "$@"
+
